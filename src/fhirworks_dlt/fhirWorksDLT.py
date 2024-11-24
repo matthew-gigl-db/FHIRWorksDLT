d999f6a5bf84a84607e08b909be5df18fa628423
matthew-gigl-db <matthew-gigl-db>
2024-11-24 07:45:09 +0000

    hard code distinct keys temp

--- src/fhirworks_dlt/fhirWorksDLT.py
+++ src/fhirworks_dlt/fhirWorksDLT.py
@@ -171,8 +171,19 @@ class ignitePipeline:
             sdf = self.spark.readStream.table(src_tbl_name)
             grouping_cols = [col for col in sdf.columns if col not in ["pos", "key", "value"]]
 
-            distinct_keys = self.spark.table(src_tbl_name).select("key").distinct().collect()
-            distinct_keys = sorted([row.key for row in distinct_keys])
+            # distinct_keys = self.spark.table(src_tbl_name).select("key").distinct().collect()
+            # distinct_keys = sorted([row.key for row in distinct_keys])
+
+              distinct_keys = ['DataModel',
+                'Destinations',
+                'EventDateTime',
+                'EventType',
+                'FacilityCode',
+                'Logs',
+                'Message',
+                'Source',
+                'Test',
+                'Transmission']
 
             return (
                 sdf
