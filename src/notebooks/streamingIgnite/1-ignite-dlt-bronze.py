04b4df5a7e079154542b1910daee4b40bdfb695d
matthew-gigl-db <matthew-gigl-db>
2024-11-26 16:03:43 +0000

    whole bunch of ignite stuff

---
+++ src/notebooks/streamingIgnite/1-ignite-dlt-bronze.py
@@ -0,0 +1,47 @@
+# Databricks notebook source
+# %pip install git+https://github.com/databrickslabs/dbignite.git
+
+# COMMAND ----------
+
+# %restart_python
+
+# COMMAND ----------
+
+import dlt
+
+# COMMAND ----------
+
+sourcePath = spark.conf.get('bundle.sourcePath')
+volume_path = spark.conf.get("workflow_inputs.volume_path")
+source_folder_path_from_volume = spark.conf.get("workflow_inputs.source_folder_path_from_volume")
+
+# COMMAND ----------
+
+import sys
+import os
+sys.path.append(os.path.abspath(f"{sourcePath}/fhirworks_dlt"))
+
+import igniteDLT as fhirWorksDLT
+
+# COMMAND ----------
+
+Pipeline = fhirWorksDLT.bronzePipeline(
+    spark = spark,
+    volume = volume_path
+)
+
+# COMMAND ----------
+
+Pipeline.raw_to_bronze(
+    table_name="fhir_bronze"
+    ,table_comment=f"A full text record of every FHIR JSON file recieved from Redox and located in {volume_path}."
+    ,table_properties={
+        "quality":"bronze"
+        ,"source":"Redox"
+        ,"pipelines.autoOptimize.managed" : "true"
+        ,"pipelines.reset.allowed" : "true"
+    }
+    ,source_folder_path_from_volume=source_folder_path_from_volume
+    ,maxFiles = 1000
+    ,maxBytes = "20g"
+)
---
+++ src/notebooks/streamingIgnite/2-ignite-dlt-silver.py
@@ -0,0 +1,68 @@
+# Databricks notebook source
+# MAGIC %pip install git+https://github.com/databrickslabs/dbignite.git
+
+# COMMAND ----------
+
+# %restart_python
+
+# COMMAND ----------
+
+import dlt
+
+# COMMAND ----------
+
+sourcePath = spark.conf.get('bundle.sourcePath')
+volume_path = spark.conf.get("workflow_inputs.volume_path")
+source_catalog = spark.conf.get("workflow_inputs.source_catalog")
+source_schema = spark.conf.get("workflow_inputs.source_schema")
+source_folder_path_from_volume = spark.conf.get("workflow_inputs.source_folder_path_from_volume")
+fhir_schemas = spark.conf.get("workflow.inputs.fhir_schemas")
+
+# COMMAND ----------
+
+# fhir_schemas = "AllergyIntolerance,Provenance,Organization,Encounter,Medication,Practitioner,Condition,RelatedPerson,DocumentReference,QuestionnaireResponse,CareTeam,Coverage,Observation,Goal,CarePlan,ServiceRequest,Questionnaire,MedicationDispense,Immunization,Patient,Specimen,Device,DiagnosticReport,Location,MedicationRequest,Procedure"
+
+# COMMAND ----------
+
+fhir_schemas = fhir_schemas.split(',')
+fhir_schemas
+
+# COMMAND ----------
+
+import sys
+import os
+sys.path.append(os.path.abspath(f"{sourcePath}/fhirworks_dlt"))
+
+import igniteDLT as fhirWorksDLT
+
+# COMMAND ----------
+
+Pipeline = fhirWorksDLT.ignitePipeline(
+    spark = spark
+    ,volume = volume_path
+)
+
+# COMMAND ----------
+
+# from dbignite.fhir_mapping_model import FhirSchemaModel
+# fhir_schema = FhirSchemaModel()
+
+# COMMAND ----------
+
+for resource in fhir_schemas:
+  Pipeline.fhir_entry(
+    bronze_table=f"{source_catalog}.{source_schema}.fhir_bronze"
+    ,fhir_resource = resource
+    ,live = False
+    ,temporary = True
+  )
+
+# COMMAND ----------
+
+for resource in fhir_schemas:
+  Pipeline.stage_silver(
+    entry_table = f"{resource}_entry"
+    ,fhir_resource = resource
+    ,live = True
+    ,temporary = False
+  )
